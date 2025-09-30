f43db87fa08ce93609e5397106812daa12f35b52
amar84777 <amar8477@gmail.com>
2025-09-30 13:28:06 +0530

    commit

--- aws/vpc.tf
+++ aws/vpc.tf
@@ -35,19 +35,4 @@ resource "aws_internet_gateway"Amar-igv" {
 
   tags = {
     Name = "lms-internet-gateway"
-  }
-}
-# Public Route Table
-resource "aws_route_table" "Amar-pub-rt" {
-  vpc_id = aws_vpc.Amar-lms.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.lms-igw.id
-  }
-
-  tags = {
-    Name = "Amar-public-rt"
-  }
-}
-
+  }}
