aa4807f9930df0a8ec34c0751b63d2f0cd8c4ba9
amar84777 <amar8477@gmail.com>
2025-09-30 12:40:17 +0530

    create

--- aws/vpc.tf
+++ aws/vpc.tf
@@ -1,8 +1,31 @@
-resource "aws_vpc" "main" {
+resource "aws_vpc" "Amar-lms" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
   tags = {
     Name = "Amar"
+  }
+}
+resource "aws_subnet" "Amr.web" {
+  vpc_id     = aws_vpc.Amar.lms.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = "true"
+  tags = {
+    Name = "websubnet"
+  }
+}
+resource "aws_subnet" "Amr.api" {
+  vpc_id     = aws_vpc.Amar.lms.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = "true"
+  tags = {
+    Name = "apisubnet"
+  }
+}
+resource "aws_subnet" "Amr.db" {
+  vpc_id     = aws_vpc.Amar.lms.id
+  cidr_block = "10.0.3.0/24"
+  tags = {
+    Name = "dbsubnet"
   }
 }
