8b80e2789bdc238be88d4979000294e5f6d2cadb
amar84777 <amar8477@gmail.com>
2025-09-30 11:34:01 +0530

    commit

---
+++ aws/variables.tf
@@ -0,0 +1,9 @@
+# Variables
+variable "aws_access_key" {
+  type = string
+  default = "Input the aws_access_key"
+}
+variable "aws_secert_key"{
+    type = string
+    default = "Input the aws_secert_key"
+}
